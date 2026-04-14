076fae81e4fe7a3f13fc4c2ad5bb4134f85ff1db
Katsiaryna Kulik <kulik_03_10@icloud.com>
2026-04-14 18:44:29 +0300

    Initial Commit

---
+++ fatsecret_SwiftUI/fatsecret_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  fatsecret_SwiftUIApp.swift
+//  fatsecret_SwiftUI
+//
+//  Created by Екатерина  on 14.04.26.
+//
+
+import SwiftUI
+
+@main
+struct fatsecret_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
