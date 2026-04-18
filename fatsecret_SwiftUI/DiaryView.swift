95652bf768ea4868331e9846736b518a53eff4ac
Katsiaryna Kulik <kulik_03_10@icloud.com>
2026-04-18 15:02:56 +0300

    Add DiaryView

---
+++ fatsecret_SwiftUI/DiaryView.swift
@@ -0,0 +1,17 @@
+//
+//  DearyView.swift
+//  fatsecret_SwiftUI
+//
+//  Created by Екатерина  on 18.04.26.
+//
+
+import SwiftUI
+
+struct DiaryView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
