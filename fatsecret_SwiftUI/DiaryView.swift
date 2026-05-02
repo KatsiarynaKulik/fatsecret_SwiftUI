254ff4e50fe93593031d9e08ac1f2d453b5f11d0
Katsiaryna Kulik <kulik_03_10@icloud.com>
2026-05-02 17:09:42 +0300

    Add: Table of proteins, fats and carbohydrates

--- fatsecret_SwiftUI/DiaryView.swift
+++ fatsecret_SwiftUI/DiaryView.swift
@@ -12,6 +12,12 @@ struct DiaryView: View {
     private let weekDates = Date().datesOfCurrentWeek()
     private let weekDays = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
 
+    // Временные данные для примера
+    @State private var proteins: Double = 25.4    // белки (граммы)
+    @State private var fats: Double = 12.8       // жиры (граммы)
+    @State private var carbs: Double = 45.2      // углеводы (граммы)
+    @State private var calories: Int = 420       // калории (ккал)
+
     var body: some View {
         VStack(spacing: 8) {
             HStack(spacing: 8) {
@@ -33,11 +39,11 @@ struct DiaryView: View {
                         .font(.system(size: 20, weight: .medium))
                         .foregroundColor(.black)
                         .frame(width: 44, height: 44)
-                        .background(Color.gray)
+                        .background(Color.gray.opacity(0.6))
                         .clipShape(.circle)
                 }
             }
-            .background(Color.red)
+           // .background(Color.red)
 
             Text(Date().formattedTodayDate())
                 .font(.system(size: 16, weight: .medium))
@@ -59,6 +65,48 @@ struct DiaryView: View {
                             .font(.system(size: 12, weight: .medium))
                             .foregroundColor(date.isPast() ? .gray.opacity(0.6) : .black)
                     }
+                }
+            }
+
+            HStack(spacing: 24) {
+                VStack(spacing: 8) {
+                    Text("Белки")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.6))
+
+                    Text(String(proteins))
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.6))
+                }
+
+                VStack(spacing: 8) {
+                    Text("Жиры")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.6))
+
+                    Text(String(fats))
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.6))
+                }
+
+                VStack(spacing: 8) {
+                    Text("Углеводы")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.6))
+
+                    Text(String(carbs))
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.6))
+                }
+
+                VStack(spacing: 8) {
+                    Text("Калории")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.9))
+
+                    Text(String(calories))
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.gray.opacity(0.9))
                 }
             }
             .padding(.horizontal)
